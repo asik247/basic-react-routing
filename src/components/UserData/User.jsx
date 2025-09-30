2f94bb32a4e2e2140bc7b4117821a3f30943f359
Asikur Rahman <mdasikur5893@gmail.com>
2025-09-30 21:03:56 +0600

    daynamice data

--- src/components/UserData/User.jsx
+++ src/components/UserData/User.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const User = ({user}) => {
     // console.log(user)
-    const {name,phone,email} = user;
+    const {id,name,phone,email} = user;
     return (
         <div className='border-2 p-4 rounded-2xl my-3'>
             <h1>{name}</h1>
             <h3>{phone}</h3>
             <p>{email}</p>
+            {/* <button className='border-2 p-2 rounded-2xl'>show details</button> */}
+            <Link to={`/user/${id}`}>show details</Link>
         </div>
     );
 };
